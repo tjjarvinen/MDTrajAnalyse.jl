82ffeff85332e9c6c82a62ebfac06e315034ad66
Teemu Järvinen <teemu.jarvinen@helsinki.fi>
2021-04-30 23:12:20 +0300

    updates for makie

--- src/SubModules/visualize.jl
+++ src/SubModules/visualize.jl
@@ -18,7 +18,7 @@ const colours = Dict(
     " N"=>:blue
     )
 
-sizes = Dict(
+const sizes = Dict(
     "C"=>0.9,
     " C"=>0.9,
     "O"=>0.9,
@@ -30,27 +30,25 @@ sizes = Dict(
     " N"=>0.9
     )
 
-function visualize(traj::AbstractTrajectory, frame; rotationspeed=0.05)
+function visualize(traj::AbstractTrajectory, frame)
     acol = map( x -> colours[x], traj.names)
     asizes = map( x -> sizes[x], traj.names)
-    sc = meshscatter(traj[frame]'; markersize=asizes,color=acol)
-    cameracontrols(sc).rotationspeed[] = rotationspeed
+    sc = meshscatter(traj[frame]; markersize=asizes,color=acol)
     return sc
 end
 
-function visualize(traj::AbstractTrajectory; step=100, rotationspeed=0.05)
+
+function visualize(traj::AbstractTrajectory; step=100)
     acol = map( x -> colours[x], traj.names)
     asizes = map( x -> sizes[x], traj.names)
-    si, i  = textslider(1:step:length(traj), "frame"; start=1)
-    x = lift( j -> traj[j][1,:], i)
-    y = lift( j -> traj[j][2,:], i)
-    z = lift( j -> traj[j][3,:], i)
-
-    sc = Scene()
-
-    meshscatter!(sc,x,y,z; markersize=asizes, color=acol)
-    cameracontrols(sc).rotationspeed[] = rotationspeed
-    final=hbox(si,sc)
+    fig = Figure()
+    ax = Axis3(fig[1,1])
+    i = Slider(fig[2,1]; range=1:step:length(traj), startvalue=1)
+    x = lift( j -> traj[j][1,:], i.value)
+    y = lift( j -> traj[j][2,:], i.value)
+    z = lift( j -> traj[j][3,:], i.value)
+    meshscatter!(ax,x,y,z; markersize=asizes, color=acol)
+    return fig
 end
 
 
